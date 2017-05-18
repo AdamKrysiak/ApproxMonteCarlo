abe9f64ecad3e612b688869d31dcbbc901847cb7
Adam Krysiak <a.krysiak15@gmail.com>
2017-05-18 17:01:19 +0200

    added score to cmake list + added random thread

--- main.cpp
+++ main.cpp
@@ -2,9 +2,47 @@
 // Created by adam on 18.05.17.
 //
 #include <iostream>
+#include <fstream>
+#include <queue>
 #include <thread>
-#include <mutex>
+#include "Score.h"
+
+void readRandom();
+
 using namespace std;
-int main(){
-	cout<<"helloWorld"<<endl;
+
+queue<long long> RANDOM_QUEUE;
+
+int main() {
+
+	readRandom();
+	srand(time(NULL));
+	cout << Score::getSumCircle1();
+	cout << rand() << endl;
+	auto x = thread(readRandom);
+	x.join();
 }
+
+void readRandom() {
+	unsigned long long int random_value = 0; //Declare value to store data into
+	size_t size = sizeof(random_value); //Declare size of data
+	ifstream urandom("/dev/urandom", ios_base::in | ios_base::binary); //Open stream
+	if (urandom) //Check if stream is open
+	{
+		while (true) {
+			urandom.read(reinterpret_cast<char *>(&random_value), size); //Read from urandom
+			if (urandom) //Check if stream is ok, read succeeded
+			{
+				RANDOM_QUEUE.push(random_value);
+			} else //Read failed
+			{
+				cerr << "Failed to read from /dev/urandom" << endl;
+			}
+		}
+		urandom.close(); //close stream
+	} else //Open failed
+	{
+		cerr << "Failed to open /dev/urandom" << endl;
+	}
+}
+
